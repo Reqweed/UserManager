e13167891d445b6aba2896dba7e0e601cb6d0cb0
Sergey Vasiliev <aragamerek@gmail.com>
2024-09-17 19:51:21 +0300

    feat: add services in pipeline

--- UserManager/Program.cs
+++ UserManager/Program.cs
@@ -1,7 +1,14 @@
+using UserManager.Extensions;
+using UserManager.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddContext(builder.Configuration);  
+builder.Services.AddIdentity();  
+builder.Services.AddMapper();  
+builder.Services.AddServices();
 builder.Services.AddRazorPages();
-
+builder.Services.AddMiddlewares();
 
 var app = builder.Build();
 
@@ -18,6 +25,7 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseMiddleware<CheckUserLockoutMiddleware>();
 
 app.MapRazorPages();
 
